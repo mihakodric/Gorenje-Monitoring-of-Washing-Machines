68c6e0c25bf39f586565835f01c3af2f05aff203
Nina Pregrad <pregrad.nina@gmail.com>
2025-08-06 11:51:36 +0200

    Update pospeskomer_odvzem_iz_bufferja_prek_mqtt_ali_wifi_ali_kabla.ino

--- pospesek/pospeskomer_odvzem_iz_bufferja_prek_mqtt_ali_wifi_ali_kabla.ino
+++ pospesek/pospeskomer_odvzem_iz_bufferja_prek_mqtt_ali_wifi_ali_kabla.ino
@@ -17,7 +17,7 @@ WebServer server(80); //privzeta povezava na port 80 v brskalniku
 
 float ax = 0, ay = 0, az = 0;
 
-const int BUFFER_SIZE = 30;  
+const int BUFFER_SIZE = 50;  
 struct Sample {                //v sample shranjuje čas, x, y, z
   unsigned long timestamp;
   float x, y, z;
