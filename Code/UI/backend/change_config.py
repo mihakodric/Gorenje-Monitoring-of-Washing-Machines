ca5237f052422549cb9118583da790a3b2353bdf
Nina Pregrad <pregrad.nina@gmail.com>
2025-09-01 15:16:01 +0200

    change config function for different sensors

--- Code/UI/backend/change_config.py
+++ Code/UI/backend/change_config.py
@@ -3,19 +3,33 @@ import json
 
 mqtt_broker = "192.168.0.106"
 mqtt_port = 1883
-topic = "acceleration/cmd"
 
-def send_config_update(param, value):
+config_updates = {
+    "acceleration/cmd": [
+        {"set": "sampling_frequency_Hz", "value": 400},
+        {"set": "sensitivity", "value": 0.000000},
+        {"set": "range_g", "value": 16},
+        {"set": "send_interval_ms", "value": 10}
+    ],
+    "current/cmd": [
+        {"set": "sampling_interval_ms", "value": 200},
+        {"set": "calibration_offset", "value": 5}
+    ],
+    "waterflow/cmd": [
+        {"set": "reset", "value": True}
+    ]
+}
+
+def send_config_update(topic, param, value):
     client = mqtt.Client()
     client.connect(mqtt_broker, mqtt_port, 60)
     message = json.dumps({"set": param, "value": value})
     client.publish(topic, message)
     client.disconnect()
-    print(f"Poslano: {message}")
+    print(f"Poslano na {topic}: {message}")
 
 if __name__ == "__main__":
+    for topic, updates in config_updates.items():
+        for update in updates:
+            send_config_update(topic, update["set"], update["value"])
 
-    send_config_update("sampling_frequency_Hz", 400)
-    send_config_update("sensitivity", 0.000000)
-    send_config_update("range_g", 16)
-    send_config_update("send_interval_ms", 10)
